c5708f67b3957a9493310623bbc25f1432516905
Radomir Stevanovic <radomir@dwavesys.com>
2018-11-08 17:23:45 -0800

    Bump version to 0.1.1 (first cross-platform release)

--- tabu/package_info.py
+++ tabu/package_info.py
@@ -14,7 +14,7 @@
 
 __packagename__ = 'dwave-tabu'
 __title__ = 'D-Wave Tabu'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'tools@dwavesys.com'
 __description__ = 'Optimized Tabu solver for QUBOs'
