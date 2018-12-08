793e76405ba60d2da87bc15634adeda821d82564
Radomir Stevanovic <radomir@dwavesys.com>
2018-12-07 23:02:08 -0800

    Bump version to 0.1.3

--- tabu/package_info.py
+++ tabu/package_info.py
@@ -14,7 +14,7 @@
 
 __packagename__ = 'dwave-tabu'
 __title__ = 'D-Wave Tabu'
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'tools@dwavesys.com'
 __description__ = 'Optimized Tabu solver for QUBOs'
