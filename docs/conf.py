dwavesystems/dwave-tabu

commit e3b590de9180193101eb76735726e2102aca6586

Fix doctest fail for sphinx2.0

docs/conf.py
@@ -38,6 +38,7 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
+    'recommonmark',
 ]
 
 autosummary_generate = True
@@ -48,13 +49,13 @@ autosummary_generate = True
 # You can specify multiple suffix as a list of string:
 #
 source_suffix = ['.rst', '.md']
-source_parsers = {'.md': 'recommonmark.parser.CommonMarkParser'}
+#source_parsers = {'.md': 'recommonmark.parser.CommonMarkParser'}
 
 # The master toctree document.
 master_doc = 'index'
 
 # Mock the C++ extension
-autodoc_mock_imports = ["tabu.tabu_search"]
+autodoc_mock_imports = ["tabu.src.tabu_search"]
 
 # Load package info, without importing the package
 basedir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))

commit 68f8e873a2371d05a20a3346658042b83cc991eb

Fix docs build on RtD (need to mock swig-generated code)
The bug was introduced in e3b590de.

If `tabu.tabu_search` file is not properly mocked while building docs
with:
```
autodoc_mock_imports = ["tabu.tabu_search"]
```
`TabuSampler` import fails (and the docstring is not compiled).

The reason we need to mock it is because otherwise we would need to
build it. And to build it, we need `swig`, which is not available on RtD
(see #25). The only alternative IIRC is to roll out our docker build
container.

docs/conf.py
@@ -55,7 +55,7 @@ source_suffix = ['.rst', '.md']
 master_doc = 'index'
 
 # Mock the C++ extension
-autodoc_mock_imports = ["tabu.src.tabu_search"]
+autodoc_mock_imports = ["tabu.tabu_search"]
 
 # Load package info, without importing the package
 basedir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))