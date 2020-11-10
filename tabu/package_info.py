99fdddb5a9ca68ea27b45b951281bc9888278e65
Radomir Stevanovic <radomir@dwavesys.com>
2020-11-10 12:27:21 -0800

    Release 0.3.1.dev0
    Changes since 0.3.0:
    
    Fixes
    ---
    
    - Publish Python 3.9 wheels for Linux/macOS/Windows

--- tabu/package_info.py
+++ tabu/package_info.py
@@ -14,7 +14,7 @@
 
 __packagename__ = 'dwave-tabu'
 __title__ = 'D-Wave Tabu'
-__version__ = '0.3.0'
+__version__ = '0.3.1.dev0'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'tools@dwavesys.com'
 __description__ = 'Optimized Tabu solver for QUBOs'
